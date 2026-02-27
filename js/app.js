efb9e0fd1a53dd3a48a2a0b4c6143e0bcd9906b2
EduardoCarlesso <eduardocarlesso@gmail.com>
2026-02-27 18:34:14 -0300

    finalizado

--- js/app.js
+++ js/app.js
@@ -1,29 +1,37 @@
 function adicionar(){
     let produto = document.getElementById("produto").value;
-    let nomeProduto = produto.split("-", 2);
-    //console.log(produto.value);
+    let nomeProduto = produto.split("-")[0];
+    let valorIndividual = produto.split("R$")[1];
     let quantidade = document.getElementById("quantidade").value;
-    let valorTotal = calcularTotal(nomeProduto[1], quantidade);
-    //console.log("n" + nomeProduto[0]);
-    let total = document.getElementById("valor-total");
-    total.innerHTML = valorTotal;
-    exibir(valorTotal, nomeProduto);
+    if(quantidade <= 0){
+        alert("Quantidade de produtos inválida! Deve ser adicionada no mínimo uma unidade");
+    }else{
+        let valorParcial = valorItens(valorIndividual, quantidade);
+        exibir(valorParcial, nomeProduto, quantidade);
+    }
 }
 
 function limpar(){
+    let escolhaProduto = document.getElementsByClassName("produto-input");
+    let quantidade = document.getElementById("quantidade").value;
     let carrinho = document.getElementById("lista-produtos");
-    carrinho.innerHTML = "";
     let total = document.getElementById("valor-total");
-    total.innerText = "";
+    escolhaProduto.innerText = "";
+    quantidade.inner = ""
+    carrinho.innerHTML = "";
+    total.innerHTML = "0";
 }
 
-function calcularTotal(item, qtd){
-    let valor = item.split("$", 2);
-    console.log(valor);
-    console.log("q:" + qtd);
-    console.log("V" + valor[1]);
-    let total = parseInt(valor[1] * qtd);
-    console.log(total);
-    return total;
+function valorItens(valor, qtd){
+    let parcial = parseInt(valor * qtd);
+    return parcial;
 }
 
+function exibir(valorParcial, nomeProduto, quantidade){
+    let total = parseInt(document.getElementById("valor-total").innerText);
+    let exibeTotal = document.getElementById("valor-total");
+    let carrinho = document.getElementById("lista-produtos");
+    let valorCarrinho = total + valorParcial;
+    exibeTotal.innerText = valorCarrinho;
+    carrinho.innerHTML += ` <section class="carrinho__produtos__produto"> <span class="texto-azul">${quantidade}x</span> ${nomeProduto}<span class="texto-azul">R$${valorParcial}</span> </section>`;
+}
